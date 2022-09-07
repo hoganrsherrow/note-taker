bd907be3673e9d923478178a1119f06b8f6307c9
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-06 19:17:29 -0500

    app.use to help with public view

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const app = express();
+
+// Allow access to public files
+app.use(express.static('./public'));
+
+// Parse incoming data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
 const port = process.env.PORT || 3001;
-const animals = require('./db/db.json');
+const notes = require('./db/db.json');
 
+// Sends index.html when url is entered
 app.get('/', (req, res) => {
-    res.json(animals);
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Sends to notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html');
+});
+
+// This routes the fetch API request from index.js to db/db.json
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// Routes POST request
+app.post('api/', (req, res) => {
+    console.log(req.body);
 })
 
-app.get('/api/db', (req, res) => {
-    res.send("Trying to access the db, eh?");
-})
 
+
+// Start listening
 app.listen(port, () => {
-    console.log(`Example app listeing on port ${port}.`);
+    console.log(`Example app listening on port ${port}.`);
 })
